d4890da22b279892bb2f7b30adaccdf796d5c563
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-09-07 13:06:56 -0300

    add handling and make the commands works

---
+++ commands/codes/bsort.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+const bsort = "returning bsort algorithm"
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('bsort')
+    .setDescription('returns a bsort algorithm!'),
+  async execute(interaction) {
+    return interaction.reply(`${bsort}`)
+  }
+}
